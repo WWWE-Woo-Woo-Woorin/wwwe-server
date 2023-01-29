2f59d406e63220bd5ab32893a01d646039278ebf
junsuPark <krpjs0508@gmail.com>
2023-01-30 06:20:28 +0900

    ✨ :: user-service module

---
+++ wwwe-server/user-service/src/main/kotlin/app/junsu/wwwe/userservice/WWWEApplication.kt
@@ -0,0 +1,8 @@
+package app.junsu.wwwe.userservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@SpringBootApplication
+@EnableJpaAuditing
+class WWWEApplication
