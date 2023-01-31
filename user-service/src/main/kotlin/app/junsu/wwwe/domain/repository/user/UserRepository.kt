ec736acbcb68a9c3fc62ac5219a9838e59763bab
junsuPark <krpjs0508@gmail.com>
2023-01-31 21:08:53 +0900

    ✨ :: implement findByEmail() in UserRepository

--- user-service/src/main/kotlin/app/junsu/wwwe/domain/repository/user/UserRepository.kt
+++ user-service/src/main/kotlin/app/junsu/wwwe/domain/repository/user/UserRepository.kt
@@ -3,4 +3,7 @@ package app.junsu.wwwe.domain.repository.user
 import app.junsu.wwwe.domain.entity.user.User
 import org.springframework.data.repository.CrudRepository
 
-interface UserRepository : CrudRepository<User, Long>
+interface UserRepository : CrudRepository<User, Long> {
+
+    fun findByEmail(email: String): User?
+}
