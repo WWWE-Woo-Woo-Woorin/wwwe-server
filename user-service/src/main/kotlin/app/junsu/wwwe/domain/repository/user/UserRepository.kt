721ed5ea5f1f69ab9f5407fce97c881620fb5bdf
junsuPark <krpjs0508@gmail.com>
2023-01-31 21:06:29 +0900

    ✨ :: implement UserRepository

---
+++ user-service/src/main/kotlin/app/junsu/wwwe/domain/repository/user/UserRepository.kt
@@ -0,0 +1,6 @@
+package app.junsu.wwwe.domain.repository.user
+
+import app.junsu.wwwe.domain.entity.user.User
+import org.springframework.data.repository.CrudRepository
+
+interface UserRepository : CrudRepository<User, Long>
