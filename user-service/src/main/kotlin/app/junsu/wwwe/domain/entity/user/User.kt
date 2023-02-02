38861bd8ce03ff72b951bf6116bcbe482efa21d6
junsuPark <krpjs0508@gmail.com>
2023-02-02 13:38:33 +0900

    ✨ :: make username in User mutable

--- user-service/src/main/kotlin/app/junsu/wwwe/domain/entity/user/User.kt
+++ user-service/src/main/kotlin/app/junsu/wwwe/domain/entity/user/User.kt
@@ -7,6 +7,6 @@ import jakarta.persistence.*
 data class User(
     @Id @GeneratedValue(strategy = GenerationType.AUTO) val id: Long? = null,
     @Column(name = "email", nullable = false, length = 64) val email: String,
-    @Column(name = "username", nullable = true, length = 8) val username: String? = null,
+    @Column(name = "username", nullable = true, length = 8) var username: String? = null,
     @Column(name = "profile_url", nullable = true, length = 1024) val profileUrl: String? = null,
 )
