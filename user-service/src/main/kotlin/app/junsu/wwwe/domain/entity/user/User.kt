681213195598e3c2a373166be2844faf31c7ce23
junsuPark <krpjs0508@gmail.com>
2023-02-02 11:40:20 +0900

    ✨ :: make User id have default null value

--- user-service/src/main/kotlin/app/junsu/wwwe/domain/entity/user/User.kt
+++ user-service/src/main/kotlin/app/junsu/wwwe/domain/entity/user/User.kt
@@ -5,7 +5,7 @@ import jakarta.persistence.*
 @Entity
 @Table(name = "user")
 data class User(
-    @Id @GeneratedValue(strategy = GenerationType.AUTO) val id: Long,
+    @Id @GeneratedValue(strategy = GenerationType.AUTO) val id: Long? = null,
     @Column(name = "email", nullable = false, length = 64) val email: String,
     @Column(name = "username", nullable = true, length = 8) val username: String? = null,
     @Column(name = "profile_url", nullable = true, length = 1024) val profileUrl: String? = null,
