74f0823fb63d5aca53417045630e5871945b9dd2
junsuPark <krpjs0508@gmail.com>
2023-02-02 12:58:51 +0900

    ✨ :: make UserRepository implements JpaRepository

--- user-service/src/main/kotlin/app/junsu/wwwe/domain/repository/user/UserRepository.kt
+++ user-service/src/main/kotlin/app/junsu/wwwe/domain/repository/user/UserRepository.kt
@@ -1,9 +1,9 @@
 package app.junsu.wwwe.domain.repository.user
 
 import app.junsu.wwwe.domain.entity.user.User
-import org.springframework.data.repository.CrudRepository
+import org.springframework.data.jpa.repository.JpaRepository
 
-interface UserRepository : CrudRepository<User, Long> {
+interface UserRepository : JpaRepository<User, Long> {
 
     fun findByEmail(email: String): User?
 }
