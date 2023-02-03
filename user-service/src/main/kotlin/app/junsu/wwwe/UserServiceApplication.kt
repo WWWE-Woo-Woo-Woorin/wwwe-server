55f8cd5d305295eaf2c08093027f4b8e8f31360b
junsuPark <krpjs0508@gmail.com>
2023-02-03 19:40:14 +0900

    ✨ :: rename Application class

--- user-service/src/main/kotlin/app/junsu/wwwe/UserServiceApplication.kt
+++ user-service/src/main/kotlin/app/junsu/wwwe/UserServiceApplication.kt
@@ -6,8 +6,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
 @EnableJpaAuditing
-class WwweApplication
+class UserServiceApplication
 
 fun main(args: Array<String>) {
-    runApplication<WwweApplication>(*args)
+    runApplication<UserServiceApplication>(*args)
 }
