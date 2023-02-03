0892fb7ed2ab19de264b29033948ed5846c8477d
junsuPark <krpjs0508@gmail.com>
2023-02-04 02:38:18 +0900

    ✨ :: make User entity extends BaseTimeEntity

--- user-service/src/main/kotlin/app/junsu/wwwe/domain/entity/user/User.kt
+++ user-service/src/main/kotlin/app/junsu/wwwe/domain/entity/user/User.kt
@@ -1,12 +1,27 @@
 package app.junsu.wwwe.domain.entity.user
 
+import app.junsu.wwwe.domain.entity.base.BaseTimeEntity
 import jakarta.persistence.*
 
 @Entity
 @Table(name = "tbl_user")
 data class User(
     @Id @GeneratedValue(strategy = GenerationType.AUTO) val id: Long? = null,
-    @Column(name = "email", nullable = false, length = 64) val email: String,
-    @Column(name = "username", nullable = true, length = 8, unique = true) var username: String? = null,
-    @Column(name = "profile_url", nullable = true, length = 1024) val profileUrl: String? = null,
-)
+    @Column(
+        name = "email",
+        nullable = false,
+        updatable = false,
+        length = 64,
+    ) val email: String,
+    @Column(
+        name = "username",
+        nullable = true,
+        length = 8,
+        unique = true,
+    ) var username: String? = null,
+    @Column(
+        name = "profile_url",
+        nullable = true,
+        length = 1024,
+    ) var profileUrl: String? = null,
+) : BaseTimeEntity()
