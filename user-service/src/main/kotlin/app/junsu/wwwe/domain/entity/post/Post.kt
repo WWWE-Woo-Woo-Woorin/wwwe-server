967842f92893fa6edb3d477b14e3fc218bc221de
junsuPark <krpjs0508@gmail.com>
2023-02-04 02:38:11 +0900

    ✨ :: make Post entity extends BaseTimeEntity

--- user-service/src/main/kotlin/app/junsu/wwwe/domain/entity/post/Post.kt
+++ user-service/src/main/kotlin/app/junsu/wwwe/domain/entity/post/Post.kt
@@ -1,8 +1,8 @@
 package app.junsu.wwwe.domain.entity.post
 
+import app.junsu.wwwe.domain.entity.base.BaseTimeEntity
 import app.junsu.wwwe.domain.entity.user.User
 import jakarta.persistence.*
-import java.time.LocalDateTime
 
 @Entity
 @Table(name = "tbl_post")
@@ -11,8 +11,7 @@ data class Post(
     @MapsId @ManyToOne(fetch = FetchType.LAZY) @JoinColumn(name = "user_id", nullable = false) val user: User,
     @Column(name = "total_likes") val totalLikes: Long,
     @Column(name = "writer", length = 8) val writer: String,
-    @Column(name = "created_at") val createdAt: LocalDateTime,
     @Column(name = "total_comments") val totalComments: Long,
     @Column(name = "content", length = 1024) val content: String,
     @Column(name = "post_image_url", length = 1024) val postImageUrl: String,
-)
+) : BaseTimeEntity()
