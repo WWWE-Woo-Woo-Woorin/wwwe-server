29404508c2b92c732b31a66a0ea1751ed3e84059
junsuPark <krpjs0508@gmail.com>
2023-02-04 03:32:32 +0900

    ➕ :: add spring boot starter security dependency

--- user-service/build.gradle.kts
+++ user-service/build.gradle.kts
@@ -4,4 +4,5 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-security:3.0.2")
 }
