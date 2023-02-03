bca740d622c35535830f850066618d5a83bc199f
junsuPark <krpjs0508@gmail.com>
2023-02-04 03:39:23 +0900

    ✨ :: implement JWTProvider functions

--- user-service/src/main/kotlin/app/junsu/wwwe/global/security/jwt/JWTProvider.kt
+++ user-service/src/main/kotlin/app/junsu/wwwe/global/security/jwt/JWTProvider.kt
@@ -4,12 +4,18 @@ import app.junsu.wwwe.model.user.token.TokenResponse
 import io.jsonwebtoken.Header
 import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.SignatureAlgorithm
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.stereotype.Component
 import java.time.LocalDateTime
 import java.util.*
 
 @Component
-class JWTProvider {
+class JWTProvider(
+    @Autowired private val userDetailsService: UserDetailsService,
+) {
 
     private val secret = "wwwe" // todo
 
@@ -36,5 +42,18 @@ class JWTProvider {
             accessToken = createAccessToken(email),
             accessTokenExp = LocalDateTime.now().plusSeconds(JWTProperties.tokenValidateTime / 1000),
         )
+    }
+
+    fun getAuthentication(token: String): Authentication {
+        val userDetails = userDetailsService.loadUserByUsername(token)
+        return UsernamePasswordAuthenticationToken(
+            userDetails,
+            "",
+            userDetails.authorities,
+        )
+    }
+
+    fun getUsername(token: String): String? {
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).body.subject
     }
 }
