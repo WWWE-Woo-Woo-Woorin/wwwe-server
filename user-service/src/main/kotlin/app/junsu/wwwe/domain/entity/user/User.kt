b0d21b8c39d455745334766af248ab14a1be2d3a
junsuPark <krpjs0508@gmail.com>
2023-02-04 00:49:14 +0900

    ✨ :: edit User table

--- user-service/src/main/kotlin/app/junsu/wwwe/domain/entity/user/User.kt
+++ user-service/src/main/kotlin/app/junsu/wwwe/domain/entity/user/User.kt
@@ -3,10 +3,10 @@ package app.junsu.wwwe.domain.entity.user
 import jakarta.persistence.*
 
 @Entity
-@Table(name = "user")
+@Table(name = "tbl_user")
 data class User(
     @Id @GeneratedValue(strategy = GenerationType.AUTO) val id: Long? = null,
     @Column(name = "email", nullable = false, length = 64) val email: String,
-    @Column(name = "username", nullable = true, length = 8) var username: String? = null,
+    @Column(name = "username", nullable = true, length = 8, unique = true) var username: String? = null,
     @Column(name = "profile_url", nullable = true, length = 1024) val profileUrl: String? = null,
 )
