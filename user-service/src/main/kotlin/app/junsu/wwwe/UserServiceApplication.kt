2b45ac47ca44020dad48d0c267a96661c9fbf9aa
junsuPark <krpjs0508@gmail.com>
2023-02-04 13:58:43 +0900

    ✨ :: add @ConfigurationPropertiesScan on UserServiceApplication

--- user-service/src/main/kotlin/app/junsu/wwwe/UserServiceApplication.kt
+++ user-service/src/main/kotlin/app/junsu/wwwe/UserServiceApplication.kt
@@ -1,11 +1,13 @@
 package app.junsu.wwwe
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
 @EnableJpaAuditing
+@ConfigurationPropertiesScan
 class UserServiceApplication
 
 fun main(args: Array<String>) {
