45209e6151caa31bb47a19f5a57defaa465b457e
junsuPark <krpjs0508@gmail.com>
2023-02-04 20:58:19 +0900

    ✨ :: add logger in SignUpController

--- wwwe/src/main/kotlin/app/junsu/wwwe/controller/user/SIgnUpController.kt
+++ wwwe/src/main/kotlin/app/junsu/wwwe/controller/user/SIgnUpController.kt
@@ -3,19 +3,23 @@ package app.junsu.wwwe.controller.user
 import app.junsu.wwwe.model.user.enter.EnterUsernameRequest
 import app.junsu.wwwe.model.user.signup.SignUpRequest
 import app.junsu.wwwe.service.user.UserService
+import mu.KotlinLogging
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/user/signup")
-class SIgnUpController(
+class SIgnUpController( // todo rename
     @Autowired private val userService: UserService,
 ) {
+
+    val logger = KotlinLogging.logger {}
 
     @PostMapping
     fun signUp(
         @RequestBody request: SignUpRequest,
     ) {
+        logger.error { request }
         userService.signUp(
             request = request,
         )
