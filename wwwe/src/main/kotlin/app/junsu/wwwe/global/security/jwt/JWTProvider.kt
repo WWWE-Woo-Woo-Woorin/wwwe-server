d37e64fd43826fa9e9e83a3f21c4275abbbb39dd
junsuPark <krpjs0508@gmail.com>
2023-02-05 09:52:42 +0900

    ✨ :: implement JWTProvider

---
+++ wwwe/src/main/kotlin/app/junsu/wwwe/global/security/jwt/JWTProvider.kt
@@ -0,0 +1,41 @@
+package app.junsu.wwwe.global.security.jwt
+
+import app.junsu.wwwe.global.security.SecurityProperties
+import app.junsu.wwwe.model.user.auth.TokenResponse
+import io.jsonwebtoken.Header
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.SignatureAlgorithm
+import org.springframework.stereotype.Component
+import java.time.LocalDateTime
+import java.util.*
+
+@Component
+class JWTProvider(
+    private val securityProperties: SecurityProperties,
+) {
+
+    private fun createAccessToken(
+        email: String,
+    ): String {
+        return Jwts.builder().signWith(
+            SignatureAlgorithm.ES256, securityProperties.secretKey,
+        ).setSubject(
+            email,
+        ).setHeaderParam(
+            Header.JWT_TYPE, JWTComponent.ACCESS,
+        ).setIssuedAt(
+            Date(),
+        ).setExpiration(
+            Date(
+                System.currentTimeMillis() + securityProperties.tokenValidTime,
+            ),
+        ).compact()
+    }
+
+    fun getToken(email: String): TokenResponse {
+        return TokenResponse(
+            accessToken = createAccessToken(email),
+            accessTokenExp = LocalDateTime.now().plusSeconds(securityProperties.tokenValidTime / 1000)
+        )
+    }
+}
