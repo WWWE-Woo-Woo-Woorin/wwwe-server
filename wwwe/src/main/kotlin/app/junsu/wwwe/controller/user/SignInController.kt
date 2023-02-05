8c340d470faa12f31413546b4ac88e85e43f49ff
junsuPark <krpjs0508@gmail.com>
2023-02-05 10:10:03 +0900

    ✨ :: implement signIn() in SignInController

--- wwwe/src/main/kotlin/app/junsu/wwwe/controller/user/SignInController.kt
+++ wwwe/src/main/kotlin/app/junsu/wwwe/controller/user/SignInController.kt
@@ -1,10 +1,24 @@
 package app.junsu.wwwe.controller.user
 
+import app.junsu.wwwe.model.user.auth.TokenResponse
+import app.junsu.wwwe.model.user.signin.SignInRequest
+import app.junsu.wwwe.service.user.UserService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/user/signin")
-class SignInController {
-        
+class SignInController(
+    @Autowired private val userService: UserService,
+) {
+
+    @PostMapping
+    fun signIn(
+        @RequestBody request: SignInRequest,
+    ): TokenResponse {
+        return userService.signIn(request)
+    }
 }
