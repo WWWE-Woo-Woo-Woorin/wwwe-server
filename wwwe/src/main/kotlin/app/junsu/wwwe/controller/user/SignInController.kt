63a1f5487d1b0d7248ce07dc928753c7cba7a221
junsuPark <krpjs0508@gmail.com>
2023-02-05 09:30:20 +0900

    ✨ :: add SignInController

---
+++ wwwe/src/main/kotlin/app/junsu/wwwe/controller/user/SignInController.kt
@@ -0,0 +1,10 @@
+package app.junsu.wwwe.controller.user
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/user/signin")
+class SignInController {
+        
+}
