3dc1f42bc87e65fd03ace7f8bc42a89939e60fa6
junsuPark <krpjs0508@gmail.com>
2023-02-05 20:59:33 +0900

    ✨ :: refactor Post entity

--- wwwe/src/main/kotlin/app/junsu/wwwe/domain/entity/post/Post.kt
+++ wwwe/src/main/kotlin/app/junsu/wwwe/domain/entity/post/Post.kt
@@ -7,11 +7,38 @@ import jakarta.persistence.*
 @Entity
 @Table(name = "tbl_post")
 data class Post(
-    @Id @GeneratedValue(strategy = GenerationType.AUTO) val id: Long? = null,
-    @MapsId @ManyToOne(fetch = FetchType.LAZY) @JoinColumn(name = "user_id", nullable = false) val user: User,
-    @Column(name = "total_likes") val totalLikes: Long,
-    @Column(name = "writer", length = 8) val writer: String,
-    @Column(name = "total_comments") val totalComments: Long,
-    @Column(name = "content", length = 1024) val content: String,
-    @Column(name = "post_image_url", length = 1024) val postImageUrl: String,
+    
+    @Id @GeneratedValue(
+        strategy = GenerationType.AUTO,
+    ) val id: Long? = null,
+
+    @MapsId @ManyToOne(
+        fetch = FetchType.LAZY,
+    ) @JoinColumn(
+        name = "user_id",
+        nullable = false,
+    ) val user: User,
+
+    @Column(
+        name = "total_likes",
+    ) val totalLikes: Long,
+
+    @Column(
+        name = "writer",
+        length = 8,
+    ) val writer: String,
+
+    @Column(
+        name = "total_comments",
+    ) val totalComments: Long,
+
+    @Column(
+        name = "content",
+        length = 1024,
+    ) val content: String,
+
+    @Column(
+        name = "post_image_url",
+        length = 1024,
+    ) val postImageUrl: String,
 ) : BaseTimeEntity()
