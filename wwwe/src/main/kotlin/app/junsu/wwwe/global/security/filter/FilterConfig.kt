360ed1524da41ed8a25521e90a21eca4ce93d67f
junsuPark <krpjs0508@gmail.com>
2023-02-05 09:51:56 +0900

    ✨ :: remove import on FilterConfig

--- wwwe/src/main/kotlin/app/junsu/wwwe/global/security/filter/FilterConfig.kt
+++ wwwe/src/main/kotlin/app/junsu/wwwe/global/security/filter/FilterConfig.kt
@@ -3,7 +3,6 @@ package app.junsu.wwwe.global.security.filter
 import app.junsu.wwwe.global.security.jwt.JWTFilter
 import app.junsu.wwwe.global.security.jwt.JWTParser
 import com.fasterxml.jackson.databind.ObjectMapper
-import org.hibernate.tool.schema.spi.ExceptionHandler
 import org.springframework.security.config.annotation.SecurityConfigurerAdapter
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.web.DefaultSecurityFilterChain
