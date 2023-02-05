75ccc2cd93befa077f928f9037e72e7730f8cf70
junsuPark <krpjs0508@gmail.com>
2023-02-05 19:24:18 +0900

    ✨ :: implement TokenController

---
+++ wwwe/src/main/kotlin/app/junsu/wwwe/controller/user/TokenController.kt
@@ -0,0 +1,24 @@
+package app.junsu.wwwe.controller.user
+
+import app.junsu.wwwe.model.user.common.TokenResponse
+import app.junsu.wwwe.model.user.token.TokenRequest
+import app.junsu.wwwe.service.user.UserService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/v1/users/token")
+internal class TokenController(
+    @Autowired private val userService: UserService,
+) {
+
+    @PutMapping
+    fun regenerateToken(
+        @RequestBody request: TokenRequest,
+    ): TokenResponse {
+        return userService.regenerateToken(request)
+    }
+}
