2607c8030f817b4e3cc78cf34cd8f2106771d7b0
junsuPark <krpjs0508@gmail.com>
2023-02-05 10:27:43 +0900

    ✨ :: change SignUpController endpoint

--- wwwe/src/main/kotlin/app/junsu/wwwe/controller/user/SignInController.kt
+++ wwwe/src/main/kotlin/app/junsu/wwwe/controller/user/SignInController.kt
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/user/signin")
+@RequestMapping("/v1/users/signin")
 class SignInController(
     @Autowired private val userService: UserService,
 ) {
