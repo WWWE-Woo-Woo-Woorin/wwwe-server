70217946ecb9d90c96f2c1c3a23193b9005721cd
junsuPark <krpjs0508@gmail.com>
2023-02-14 16:58:31 +0900

    ✨ :: implement fetchUserInformation()

--- wwwe/src/main/kotlin/app/junsu/wwwe/service/user/UserService.kt
+++ wwwe/src/main/kotlin/app/junsu/wwwe/service/user/UserService.kt
@@ -3,8 +3,10 @@ package app.junsu.wwwe.service.user
 import app.junsu.wwwe.domain.entity.user.User
 import app.junsu.wwwe.domain.repository.user.UserRepository
 import app.junsu.wwwe.exception.ServerException.*
+import app.junsu.wwwe.global.security.SecurityFacade
 import app.junsu.wwwe.global.security.jwt.JWTProvider
 import app.junsu.wwwe.model.user.common.TokenResponse
+import app.junsu.wwwe.model.user.fetch.FetchUserInformationResponse
 import app.junsu.wwwe.model.user.signin.SignInRequest
 import app.junsu.wwwe.model.user.signup.SignUpRequest
 import app.junsu.wwwe.model.user.token.TokenRequest
@@ -17,6 +19,7 @@ import org.springframework.web.bind.annotation.ResponseStatus
 @Service
 class UserService(
     @Autowired private val userRepository: UserRepository,
+    @Autowired private val securityFacade: SecurityFacade,
     @Autowired private val jwtProvider: JWTProvider,
 ) {
 
@@ -55,8 +58,6 @@ class UserService(
     ): TokenResponse {
 
         val user = userRepository.findByEmail(request.email) ?: throw UserNotFoundException()
-
-        user.saveDeviceToken(request.deviceToken)
 
         return jwtProvider.getToken(user.email)
     }
@@ -105,5 +106,17 @@ class UserService(
         val user = userRepository.findByEmail(email) ?: throw UserNotFoundException()
 
         return user.name != null
+    }
+
+    @Transactional(readOnly = true)
+    fun fetchUserInformation(): FetchUserInformationResponse {
+
+        val user = securityFacade.getCurrentUser()
+
+        return FetchUserInformationResponse(
+            email = user.email,
+            username = user.username,
+            profileUrl = user.profileUrl,
+        )
     }
 }
