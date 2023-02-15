9c452ca116157995f856b333674447be0b0f9af4
junsuPark <krpjs0508@gmail.com>
2023-02-15 15:50:10 +0900

    ✨ :: implement PostType parsing method

--- wwwe/src/main/kotlin/app/junsu/wwwe/domain/entity/post/PostType.kt
+++ wwwe/src/main/kotlin/app/junsu/wwwe/domain/entity/post/PostType.kt
@@ -2,13 +2,13 @@ package app.junsu.wwwe.domain.entity.post
 
 enum class PostType {
     DEFAULT, MAJOR, CLUB, ;
+}
 
-    fun String.toPost(): PostType {
-        return when (this) {
-            "DEFAULT" -> DEFAULT
-            "MAJOR" -> MAJOR
-            "CLUB" -> CLUB
-            else -> throw IllegalArgumentException()
-        }
+fun String.toPost(): PostType {
+    return when (this) {
+        "DEFAULT" -> PostType.DEFAULT
+        "MAJOR" -> PostType.MAJOR
+        "CLUB" -> PostType.CLUB
+        else -> throw IllegalArgumentException()
     }
 }
--- wwwe/src/main/kotlin/app/junsu/wwwe/model/post/CreatePostRequest.kt
+++ wwwe/src/main/kotlin/app/junsu/wwwe/model/post/CreatePostRequest.kt
@@ -3,4 +3,5 @@ package app.junsu.wwwe.model.post
 data class CreatePostRequest(
     val postImageUrl: String,
     val content: String,
+    val postType: String,
 )
