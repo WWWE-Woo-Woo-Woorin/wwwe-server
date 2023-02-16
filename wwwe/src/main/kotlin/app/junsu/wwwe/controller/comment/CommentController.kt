aaf70074ed7e1693443347991fa4bf65c5fd3e5c
junsuPark <krpjs0508@gmail.com>
2023-02-16 14:53:45 +0900

    ✨ :: implement CommentController

---
+++ wwwe/src/main/kotlin/app/junsu/wwwe/controller/comment/CommentController.kt
@@ -0,0 +1,23 @@
+package app.junsu.wwwe.controller.comment
+
+import app.junsu.wwwe.model.comment.CreateCommentRequest
+import app.junsu.wwwe.service.comment.CommentService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/v1/posts/{post-id}/comments")
+private class CommentController(
+    @Autowired private val commentService: CommentService,
+) {
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    private fun createComment(
+        @PathVariable("post-id") parameter: String,
+        @RequestBody request: CreateCommentRequest,
+    ) {
+
+    }
+}
