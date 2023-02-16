22704dd5a438afa1c6fda503979bdb520a33648f
junsuPark <krpjs0508@gmail.com>
2023-02-16 16:42:27 +0900

    ✨ :: implement findPostById() in PostRepository

--- wwwe/src/main/kotlin/app/junsu/wwwe/domain/repository/post/PostRepository.kt
+++ wwwe/src/main/kotlin/app/junsu/wwwe/domain/repository/post/PostRepository.kt
@@ -3,4 +3,7 @@ package app.junsu.wwwe.domain.repository.post
 import app.junsu.wwwe.domain.entity.post.Post
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface PostRepository : JpaRepository<Post, Long>
+interface PostRepository : JpaRepository<Post, Long> {
+
+    fun findPostById(id: Long): Post?
+}
