307ce5ccd61281cfe13a2e84136a2fcb42d2948e
junsuPark <krpjs0508@gmail.com>
2023-02-16 14:53:26 +0900

    ✨ :: implement CommentRepository

---
+++ wwwe/src/main/kotlin/app/junsu/wwwe/domain/repository/comment/CommentRepository.kt
@@ -0,0 +1,6 @@
+package app.junsu.wwwe.domain.repository.comment
+
+import app.junsu.wwwe.domain.entity.comment.Comment
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface CommentRepository : JpaRepository<Comment, Long>
