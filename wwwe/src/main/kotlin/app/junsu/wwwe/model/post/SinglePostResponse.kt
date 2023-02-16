3b852d07d2e1ce238e0a242dc95ce5b83f6a18c9
junsuPark <krpjs0508@gmail.com>
2023-02-16 19:25:13 +0900

    ✨ :: implement List<Post>.toResponse(): List<PostResponse>

--- wwwe/src/main/kotlin/app/junsu/wwwe/model/post/SinglePostResponse.kt
+++ wwwe/src/main/kotlin/app/junsu/wwwe/model/post/SinglePostResponse.kt
@@ -1,8 +1,24 @@
 package app.junsu.wwwe.model.post
 
-data class PostResponse(
+import app.junsu.wwwe.domain.entity.post.Post
+import java.time.LocalDateTime
+
+internal data class PostResponse(
     val postId: Long,
     val writer: String,
     val content: String,
     val postImageUrl: String,
+    val createdAt: LocalDateTime,
 )
+
+internal fun List<Post>.toResponse(): List<PostResponse> {
+    return this.map {
+        PostResponse(
+            postId = it.id!!,
+            writer = it.user.username,
+            content = it.content,
+            postImageUrl = it.postImageUrl,
+            createdAt = it.createdAt!!,
+        )
+    }
+}
