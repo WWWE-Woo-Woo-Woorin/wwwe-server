3cdce098be53ee64963c6efe2bebc0ab2545b3ff
junsuPark <krpjs0508@gmail.com>
2023-02-16 22:31:00 +0900

    ✨ :: add method in CommentRepository

--- wwwe/src/main/kotlin/app/junsu/wwwe/domain/repository/comment/CommentRepository.kt
+++ wwwe/src/main/kotlin/app/junsu/wwwe/domain/repository/comment/CommentRepository.kt
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository
 
 interface CommentRepository : JpaRepository<Comment, Long> {
 
-    fun findAllByPostId(postId: Long): List<Comment>
+    fun findCommentById(commentId: Long): Comment?
+
+    fun findAllByPostId(postId: Long): List<Comment?>
 }
