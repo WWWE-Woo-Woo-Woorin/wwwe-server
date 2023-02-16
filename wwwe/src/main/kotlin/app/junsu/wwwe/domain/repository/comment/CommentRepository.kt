3f7a3d6cb41d2442f7774935d2e779264608b552
junsuPark <krpjs0508@gmail.com>
2023-02-16 22:05:54 +0900

    ✨ :: implement method inquiring all of comments in CommentRepository

--- wwwe/src/main/kotlin/app/junsu/wwwe/domain/repository/comment/CommentRepository.kt
+++ wwwe/src/main/kotlin/app/junsu/wwwe/domain/repository/comment/CommentRepository.kt
@@ -3,4 +3,7 @@ package app.junsu.wwwe.domain.repository.comment
 import app.junsu.wwwe.domain.entity.comment.Comment
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface CommentRepository : JpaRepository<Comment, Long>
+interface CommentRepository : JpaRepository<Comment, Long> {
+
+    fun findAllByPostId(postId: Long): List<Comment>
+}
